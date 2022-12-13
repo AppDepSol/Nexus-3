4d6284221fb6a5e964d8a4b82500c84e5ecc2274
AppDepSol <appdepsol@gmail.com>
2022-12-13 09:50:04 +0000

    Version 1

---
+++ test.html
@@ -0,0 +1,9 @@
+print("Hello-World")
+
+echo "Hello-World"
+
+echo 'Jesus is Lord'
+
+echo 'DevOps is truly an enviable career'
+
+echo "Study Devops now"
---
+++ test.py
@@ -0,0 +1,23 @@
+test.txt is going to be great
+
+In PYTHON scripting, print is used to print data
+
+#print("Hello-World")
+
+#print("Jesus is Lord")
+
+#print("DevOps is truly an enviable career")
+
+#print("Study Devops now")
+
+
+A script is a command (a collection of commands)
+Combined scripts can be pasted at once.
+
+echo "Hello-World"
+
+echo 'Jesus is Lord'
+
+echo 'DevOps is truly an enviable career'
+
+echo "Study Devops now"
